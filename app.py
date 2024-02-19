24060ed55bbb9b110d500bf30ceaa147b250bc4b
kerwenard <fang1997313@gmail.com>
2024-02-19 19:52:44 +0800

    🌈 style: 改一些变量名

--- app.py
+++ app.py
@@ -11,15 +11,19 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 def thread_download_image():
     while True:
-        task = database.get_download_task()
-        if task is None:
-            time.sleep(3)
-            continue
-        print(task.url)
-        response = requests.get(task.url)
-        with open(f"./temp/{task.file_name}", "wb") as file:
-            file.write(response.content)
-        database.del_download_task(task)
+        try:
+            task = database.get_download_task()
+            if task is None:
+                time.sleep(3)
+                continue
+            print(task.url)
+            response = requests.get(task.url)
+            if response.status_code == 200:
+                with open(f"./temp/{task.file_name}", "wb") as file:
+                    file.write(response.content)
+                database.del_download_task(task)
+        except:
+            pass
 
 
 if __name__ == "__main__":
@@ -27,7 +31,7 @@ if __name__ == "__main__":
     database.init()
     last_page = database.get_last_page()
     scheduler = BackgroundScheduler()
-    for i in range(10):  # 对应开启的下载进程数量
+    for i in range(3):  # 对应开启的下载进程数量，如果太频繁会导致服务器拒绝响应
         scheduler.add_job(thread_download_image, "date", next_run_time=datetime.now())
     scheduler.start()
 
@@ -44,24 +48,25 @@ if __name__ == "__main__":
         ) is not None:
             print(last_page)
             # 检查商品url
-            for ele_button in driver.wait_elements_by_xpath(
+            for ele_commodity_button in driver.wait_elements_by_xpath(
                 '//ul/li[@class="newly-added-items__item nomore"]/a'
             ):
-                url = ele_button.get_property("href")
+                url = ele_commodity_button.get_property("href")
                 commodity_key = url.split("=")[-1]
                 if database.check_commodity_key(commodity_key):
                     print("商品已被抓取")
                     continue
+
                 # 图片页
                 driver.execute_script(f'window.open("{url}", "_blank");')
                 time.sleep(3)
                 driver.switch_to.window(driver.window_handles[1])
                 if (
-                    button := driver.wait_element_by_xpath(
+                    ele_btn_show_all_pic := driver.wait_element_by_xpath(
                         '//*[@id="__layout"]/div/div[1]/div[2]/div/div/div/div/div/section[1]/div/div[2]/div[2]/p/a'
                     )
                 ) is not None:
-                    button.click()
+                    ele_btn_show_all_pic.click()
                     for ele_image in driver.wait_elements_by_xpath(
                         '//*[@id="__layout"]/div/div[1]/div[2]/div/div/section/ul/li/img'
                     ):
